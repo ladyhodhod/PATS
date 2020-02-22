245bcd7a70f1fe769bbeb43e0b6664883ee30d72
ladyhodhod <ladyhodhod@gmail.com>
2020-02-22 20:35:38 +0300

    added factories.rb file

--- test/models/pet_test.rb
+++ test/models/pet_test.rb
@@ -4,4 +4,12 @@ class PetTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test ​"pet attributes must not be empty"​​ do​
+  ​ 	  pet = Pet.new
+  ​ 	  assert pet.invalid?
+  ​ 	  assert pet.errors[​:name].any?
+  ​ 	  assert pet.errors[​:owner_id].any?
+  ​     assert pet.errors[​:animal_id].any?
+  end
 end
