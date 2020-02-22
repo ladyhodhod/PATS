c6ad703a18173564f7b44e0fc733ef9c2b8864df
ladyhodhod <ladyhodhod@gmail.com>
2020-02-22 23:36:05 +0300

    all is commited

--- test/factories.rb
+++ test/factories.rb
@@ -1,16 +1,16 @@
 # Faker: creates a fake names, adresses
 FactoryBot.define do
-    factory :owner do
-        first_name {"Alex"}
-        last_name {"Heimann"}
-        street {"10152 Sudberry Drive"}
-        city {"Wexford"}
-        state {"PA"}
-        zip {"15090"}
-        active {true}
-        phone {rand(10 ** 10).to_s.rjust(10,'0') }
-        email {|a| "#{a.first_name}.#{a.last_name}@example.com".downcase }
-    end
+  factory :owner do
+    first_name {"Alex"}
+    last_name {"Heimann"}
+    street {"10152 Sudberry Drive"}
+    city {"Wexford"}
+    state {"PA"}
+    zip {"15090"}
+    active {true}
+    phone {rand(10 ** 10).to_s.rjust(10,'0') }
+    email {|a| "#{a.first_name}.#{a.last_name}@example.com".downcase }
+  end
 
     factory :animal do
       name "Cat"
