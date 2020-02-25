733171d660f6a0b3d46acc45798d54556310f06e
ladyhodhod <ladyhodhod@gmail.com>
2020-02-25 09:49:02 +0300

    added fixtures to be deleted

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,7 +1,7 @@
 class Owner < ApplicationRecord
 
     # Callbacks
-
+    before_save :reformat_phone
     # Relationships
     # -----------------------------
     has_many :pets
@@ -30,8 +30,21 @@ class Owner < ApplicationRecord
   # email format (other regex for email exist; doesn't allow .museum, .aero, etc.)
   # Not allowing for .uk, .ca, etc. because this is a Pittsburgh business and customers not likely to be out-of-country
   validates_format_of :email, with: /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i, message: "is not a valid format"
-  
+  def name
+    "#{last_name}, #{first_name}"
+  end
 
-
+  def proper_name
+    "#{first_name} #{last_name}"
+  end
     
+  
+ private 
+  def reformat_phone
+    p= self.phone.to_s
+    p.gsub(/[^0-9]/,'')
+    self.phone=p
+  end
+
+
 end
--- test/models/owner_test.rb
+++ test/models/owner_test.rb
@@ -2,37 +2,22 @@ require 'test_helper'
 
 class OwnerTest < ActiveSupport::TestCase
 
-   test "the truth" do
-    assert true
-   end
-
-
-
-
-
-
-
- # Test Relationships
-
-
-
-  # Test Validations
-
-
-
-
-
-
-
-
+  #  test "the truth" do
+  #   assert true
+  #  end
 
   # We use a library called shoulda-matchers to test validations and associations. 
-  # shoulda-matchers provides matchers for writing single line tests for common Rails functionality. 
-
-
- 
+  # shoulda-matchers provides matchers for writing single line tests for common Rails functionality.
+  
+  # Test Relationships
+  should have_many(:pets)
+  should have_many(:visits).through(:pets)
 
   # Test Validations
+  should validate_presence_of(:first_name)
+  should validate_presence_of(:last_name)
+  should validate_presence_of(:email)
+  should validate_presence_of(:phone)
 
   # Test Scopes
 
