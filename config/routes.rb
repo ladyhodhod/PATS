a0a30903ae376780b5a132b10f9542666b33ec14
ladyhodhod <ladyhodhod@gmail.com>
2020-03-08 09:44:33 +0300

    deleted Pets Controllers and updated the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   resources :owners
-  resources :pets
-  resources :animals
-  resources :visits
+  
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
